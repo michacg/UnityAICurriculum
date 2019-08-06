e932ddd1f54623613a58226355047308ac743061
michacg <michacg2@uci.edu>
2019-08-06 14:17:32 -0700

    Lesson 03 DONE

--- AI Curriculum Project/Assets/Scripts/HelperController.cs
+++ AI Curriculum Project/Assets/Scripts/HelperController.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class HelperController : MonoBehaviour
 {
+    //State for our Finite State Machine
+    enum State
+    {
+        follow,
+        pickup
+    };
 
     public GameObject player;
     public float targetDistance;
-    public float allowedDistance = 3f;
+    public float allowedDistance = 1f;
     public float speed;
+    public float lookRadius = 5f;
     public RaycastHit hit;
 
     private Animator anim;
+    private State currentState = State.follow;
+    private Collider[] objectsAround;
+    private GameObject currentPickup;
 
     void Start()
     {
@@ -20,6 +30,21 @@ public class HelperController : MonoBehaviour
 
 
     void Update()
+    {
+        LookAround();
+
+        switch(currentState)
+        {
+            case State.follow:
+                FollowUpdate();
+                break;
+            case State.pickup:
+                PickupUpdate();
+                break;
+        }
+    }
+
+    void FollowUpdate()
     {
         transform.LookAt(player.transform);
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
@@ -35,6 +60,38 @@ public class HelperController : MonoBehaviour
             {
                 speed = 0f;
             }
+        }
+    }
+
+    void PickupUpdate()
+    {
+        transform.LookAt(currentPickup.transform);
+        speed = 0.03f;
+        anim.Play("walk");
+        transform.position = Vector3.MoveTowards(transform.position, currentPickup.transform.position, speed);
+    }
+
+    void LookAround()
+    {
+        objectsAround = Physics.OverlapSphere(GetComponent<Transform>().position, lookRadius);
+        for (int i = 0; i < objectsAround.Length; ++i)
+        {
+            if (objectsAround[i].gameObject.CompareTag("pickup"))
+            {
+                currentState = State.pickup;
+                currentPickup = objectsAround[i].gameObject;
+                break;  
+            }
+        }
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("pickup"))
+        {
+            Destroy(col.gameObject);
+            currentState = State.follow;
         }
     }
 }
