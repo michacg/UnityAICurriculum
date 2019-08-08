6afa7ae0d2d10c58f6061382a31965799b0574af
Michaela Gonzales <michacg2@uci.edu>
2019-08-08 12:43:11 -0700

    Lesson 7,8 DONE

---
+++ AI Curriculum Project/Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Pathfinder : MonoBehaviour
+{
+    public GameObject target;
+    NavMeshAgent agent;
+    Animator anim;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.transform.position, transform.position);
+
+        if (distance < 5)
+        {
+            anim.Play("Hit");
+        }
+        else
+        {
+            anim.Play("Run");
+            agent.SetDestination(target.transform.position);
+        }
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("player"))
+        {
+            Debug.Log("Pushed!");
+            //Have students have something happen.
+        }
+    }
+}
