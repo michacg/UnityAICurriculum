2db20a70063077723bfadb48cefa326000129fd6
Michaela Gonzales <michacg2@uci.edu>
2019-08-07 19:56:27 -0700

    Lesson 06 don4e

---
+++ AI Curriculum Project/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class EnemyController : MonoBehaviour
+{
+    public float lookRadius = 5f;
+
+    public Transform player;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        float distance = Vector3.Distance(player.position, transform.position);
+        transform.LookAt(player.transform);
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(player.position);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("player"))
+        {
+            Debug.Log("You Died.");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    //This doesn't do anything to our game, just for debugging purposes!
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
